5d85f08baec75d6013baae543cda89777e0af840
haeseonkim <khs51777@naver.com>
2024-12-18 22:54:00 +0900

    test: 유저 포인트 조회시 실패 테스트 코드 추가

---
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -0,0 +1,44 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.exception.UserPointNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PointServiceTest {
+
+    @Mock
+    private PointHistoryTable pointHistoryTable;
+
+    @Mock
+    private UserPointTable userPointTable;
+
+    @InjectMocks
+    private PointService pointService;
+
+    @Nested
+    @DisplayName("유저 포인트 조회 서비스 테스트")
+    class getUserPointTest{
+        @Test
+        void 유저_포인트_조회시_없을때_UserPointNotFoundException() {
+            // given
+            long userId = 1L;
+
+            // when
+            when(userPointTable.selectById(userId)).thenReturn(null);
+
+            // then
+            assertThrows(UserPointNotFoundException.class, () -> pointService.getUserPoint(userId));
+        }
+    }
+}
