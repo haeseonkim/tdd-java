48e87d4e96e5edcab8a6335bedb360a352849b7a
haeseonkim <khs51777@naver.com>
2024-12-19 23:01:02 +0900

    refactor: 컨트롤러에서 서비스 메서드 호출하도록 수정

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,14 +1,18 @@
 package io.hhplus.tdd.point;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/point")
 public class PointController {
+
+    private final PointServiceFacade pointServiceFacade;
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
 
@@ -19,7 +23,7 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointServiceFacade.getUserPoint(id);
     }
 
     /**
@@ -29,7 +33,7 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return pointServiceFacade.getPointHistory(id);
     }
 
     /**
@@ -40,7 +44,7 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointServiceFacade.chargeUserPoint(id, amount);
     }
 
     /**
@@ -51,6 +55,6 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointServiceFacade.unChargeUserPoint(id, amount);
     }
 }
