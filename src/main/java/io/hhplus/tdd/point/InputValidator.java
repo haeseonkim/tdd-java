3e7dae9eb705a25f81c65e767a0f66c29ca1b750
haeseonkim <khs51777@naver.com>
2024-12-19 22:41:41 +0900

    refactor: 메서드 재사용하도록 수정

--- src/main/java/io/hhplus/tdd/point/InputValidator.java
+++ src/main/java/io/hhplus/tdd/point/InputValidator.java
@@ -11,9 +11,7 @@ public class InputValidator {
     }
 
     public void checkInputValue(Long userId, Long amount){
-        if (userId <= 0) {
-            throw new IllegalArgumentException("userId must be greater than 0");
-        }
+        checkInputValue(userId);
 
         if (amount == null) {
             throw new IllegalArgumentException("amount cannot be null");
