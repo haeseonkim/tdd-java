84133569146aa53458ad14260c79c4082261918b
haeseonkim <khs51777@naver.com>
2024-12-19 21:21:25 +0900

    feat: PointHistoryService의 save 메서드에도 ConcurrentHashMap + ReentrantLock 추가

--- src/main/java/io/hhplus/tdd/point/service/PointHistoryService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointHistoryService.java
@@ -7,18 +7,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
 public class PointHistoryService {
     private final PointHistoryTable pointHistoryTable;
+    private final ConcurrentHashMap<Long, ReentrantLock> pointHistoryLock = new ConcurrentHashMap<>();
 
     public List<PointHistory> getPointHistory(long userId) {
         return pointHistoryTable.selectAllByUserId(userId);
     }
 
     public void savePointHistory(long userId, long point, TransactionType transactionType) {
-        pointHistoryTable.insert(userId, point, transactionType, System.currentTimeMillis());
+        ReentrantLock lock = pointHistoryLock.computeIfAbsent(userId, k -> new ReentrantLock());
 
+        try{
+            lock.lock();
+
+            pointHistoryTable.insert(userId, point, transactionType, System.currentTimeMillis());
+        }finally {
+            lock.unlock();
+        }
     }
 }
