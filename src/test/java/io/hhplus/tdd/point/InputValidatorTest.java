7e72568ec508632aab13133a054623ed46de8a0f
haeseonkim <khs51777@naver.com>
2024-12-19 22:34:08 +0900

    test: 입력값 검증 시 userId가 0 이하일때 실패 테스트 코드 추가

---
+++ src/test/java/io/hhplus/tdd/point/InputValidatorTest.java
@@ -0,0 +1,27 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class InputValidatorTest {
+    @Nested
+    @DisplayName("userId에 대한 입력값 테스트")
+    class CheckInputValueTest{
+        private InputValidator inputValidator = new InputValidator();
+
+        @Test
+        void userId_값이_0_이하_일때_IllegalArgumentException(){
+            // given
+            long userId = -1L;
+
+            // when & then
+            assertThrows(IllegalArgumentException.class, () -> inputValidator.checkInputValue(userId));
+        }
+    }
+}
