a25f2ca70e12bbd3f45370c1c4ede31578c416aa
haeseonkim <khs51777@naver.com>
2024-12-19 17:33:44 +0900

    refactor: UserPoint, PointHistory 구분해서 facade 패턴으로 리팩토링

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,10 +1,7 @@
 package io.hhplus.tdd.point;
 
-import io.hhplus.tdd.database.PointHistoryTable;
-import io.hhplus.tdd.database.UserPointTable;
-import io.hhplus.tdd.point.exception.NotEnoughPointException;
-import io.hhplus.tdd.point.exception.PointLimitExceededException;
-import io.hhplus.tdd.point.exception.UserPointNotFoundException;
+import io.hhplus.tdd.point.facade.PointHistoryFacade;
+import io.hhplus.tdd.point.facade.UserPointFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,60 +10,33 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class PointService {
-    private final PointHistoryTable pointHistoryTable;
-    private final UserPointTable userPointTable;
+    private final UserPointFacade userPointFacade;
+    private final PointHistoryFacade pointHistoryFacade;
 
-    public static final long MAXIMUM_POINT_LIMIT = 100 * 100 * 100;
-
-    public UserPoint getUserPoint(Long userId) {
-        UserPoint userPoint = userPointTable.selectById(userId);
-        if(userPoint == null) {
-            throw new UserPointNotFoundException(userId);
-        }
-        return userPoint;
+    public UserPoint getUserPoint(long userId) {
+        return userPointFacade.getUserPoint(userId);
     }
 
-    public List<PointHistory> getPointHistory(Long userId) {
-        return pointHistoryTable.selectAllByUserId(userId);
+    public List<PointHistory> getPointHistory(long userId) {
+        return pointHistoryFacade.getPointHistory(userId);
     }
 
-    public UserPoint chargeUserPoint(Long userId, Long newPoint) {
+    public UserPoint chargeUserPoint(long userId, long newPoint) {
         // 1. 포인트 충전
-        UserPoint userPoint = userPointTable.selectById(userId);
-        UserPoint updatedPoint;
-
-        if (userPoint == null) {
-            updatedPoint = userPointTable.insertOrUpdate(userId, newPoint);
-        } else {
-            long totalPoint = userPoint.point() + newPoint;
-            if (totalPoint > MAXIMUM_POINT_LIMIT) {
-                throw new PointLimitExceededException(userId);
-            }
-            updatedPoint = userPointTable.insertOrUpdate(userId, totalPoint);
-        }
+        UserPoint updatedPoint = userPointFacade.chargeUserPoint(userId, newPoint);
 
         // 2. 히스토리 insert
-        pointHistoryTable.insert(userId, newPoint, TransactionType.CHARGE, System.currentTimeMillis());
+        pointHistoryFacade.savePointHistory(userId, newPoint, TransactionType.CHARGE);
 
         return updatedPoint;
     }
 
-    public UserPoint unChargeUserPoint(Long userId, Long newPoint) {
+    public UserPoint unChargeUserPoint(long userId, long usePoint) {
         // 1. 포인트 사용
-        UserPoint userPoint = userPointTable.selectById(userId);
-        UserPoint updatedPoint;
-        if (userPoint == null) {
-            throw new UserPointNotFoundException(userId);
-        }else {
-            long totalPoint = userPoint.point() - newPoint;
-            if(totalPoint < 0) {
-                throw new NotEnoughPointException(userId);
-            }
-            updatedPoint = userPointTable.insertOrUpdate(userId, totalPoint);
-        }
+        UserPoint updatedPoint = userPointFacade.unChargeUserPoint(userId, usePoint);
 
         // 2. 히스토리 insert
-        pointHistoryTable.insert(userId, newPoint, TransactionType.USE, System.currentTimeMillis());
+        pointHistoryFacade.savePointHistory(userId, usePoint, TransactionType.USE);
 
         return updatedPoint;
     }
---
+++ src/main/java/io/hhplus/tdd/point/facade/PointHistoryFacade.java
@@ -0,0 +1,24 @@
+package io.hhplus.tdd.point.facade;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PointHistoryFacade {
+    private final PointHistoryTable pointHistoryTable;
+
+    public List<PointHistory> getPointHistory(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+    public void savePointHistory(long userId, long point, TransactionType transactionType) {
+        pointHistoryTable.insert(userId, point, transactionType, System.currentTimeMillis());
+
+    }
+}
